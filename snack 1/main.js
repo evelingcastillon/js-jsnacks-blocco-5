feb293c1613e24a70b7a665ccf751607582d8c03
evelingcastillon <evelingcastillon0304@gmail.com>
2021-05-11 09:53:44 +0200

    correzione nel ciclo for i

--- snack 1/main.js
+++ snack 1/main.js
@@ -5,22 +5,22 @@ Calcola quanto pesano tutte le zucchine. */
 
 var zucchina = [
     { varieta : 'Zucchino nero', peso : 100, lunghezza : 21},
-    { varieta : 'Zucchino tondo', peso : 100, lunghezza : 23},
+    { varieta : 'Zucchino tondo', peso : 180, lunghezza : 23},
     { varieta : 'Zucchino romanesco', peso : 100, lunghezza : 21},
-    { varieta : 'Zucchina trombetta', peso : 100, lunghezza : 20},
+    { varieta : 'Zucchina trombetta', peso : 200, lunghezza : 20},
     { varieta : 'Zucchina bianca', peso : 100, lunghezza : 21},
-    { varieta : 'Zucchino giallo', peso : 100, lunghezza : 22},
+    { varieta : 'Zucchino giallo', peso : 150, lunghezza : 22},
     { varieta : 'Zucchino giallo rugoso', peso : 100, lunghezza : 20},
-    { varieta : 'Zucchina Crookneck', peso : 100, lunghezza : 20},
-    { varieta : 'Zucchina rigata', peso : 100, lunghezza : 22},
+    { varieta : 'Zucchina Crookneck', peso : 170, lunghezza : 20},
+    { varieta : 'Zucchina rigata', peso : 175, lunghezza : 22},
     { varieta : 'Zucchina striata', peso : 100, lunghezza : 23},
 ];
-
-console.log(zucchina[1].lunghezza );
+/* 
+console.log(zucchina[1].lunghezza ); */
 
 var somma = 0;
-for(var i = 0; 0 < zucchina.length; i++) {
-    var zucchinaEl = zucchina[i].lunghezza;
-    somma = somma + zucchinaEl;
-    console.log(somma);
+for(var i = 0; i < zucchina.length; i++) {
+    var zucchinaEl = zucchina[i];
+    somma += zucchinaEl.peso;
 }
+console.log(somma);
